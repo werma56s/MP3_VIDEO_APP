b539328e8aff8df528ccb70eaceae8954be424d1
werka <werma56s@gmail.com>
2020-05-28 17:59:55 +0200

    Wersja_ostateczna_aplikacji

---
+++ PlayerMP3AndVideoTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PlayerMP3AndVideoTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PlayerMP3AndVideoTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("157e5ca3-2dfa-471b-bde8-12c6bb018012")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlayerMP3AndVideo;
+
+namespace PlayerMP3AndVideoTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetMd5Hash()
+        {
+            MD5 hash = MD5.Create();
+            string pass = "Hello World!";
+
+            string result = LogPanel.GetMd5Hash(hash, pass);
+
+            string Expected = "ed076287532e86365e841e92bfc50d8c";
+  
+            Assert.AreEqual(Expected, result, "nie sa rowne");
+
+        }
+        [TestMethod]
+        public void TestVerifyMd5Hash()
+        {
+            MD5 md5Hash = MD5.Create();
+            string pass = "Hello World!";
+
+            string hash = LogPanel.GetMd5Hash(md5Hash, pass);
+
+            bool result = LogPanel.VerifyMd5Hash(md5Hash, pass, hash);
+
+            bool Expected = true;
+
+            Assert.AreEqual(Expected, result, "nie sa rowne");
+
+        }
+        
+    }
+
+}
